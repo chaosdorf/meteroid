6853d3dabed6f57cf4b36de1b86ee9dc8da6696c
Niklas Sombert <niklas@ytvwld.de>
2016-06-22 14:45:37 +0200

    Don't require entering a balance when creating a new user.

--- meteroid/src/main/java/de/chaosdorf/meteroid/AddUserActivity.java
+++ meteroid/src/main/java/de/chaosdorf/meteroid/AddUserActivity.java
@@ -90,21 +90,19 @@ public class AddUserActivity extends Activity implements LongRunningIOCallback
 					emailValue = email.toString();
 				}
 
+				double balanceValue = 0;
 				final CharSequence balance = balanceText.getText();
-				if (balance == null || balance.length() == 0)
+				if (balance != null && balance.length() > 0)
 				{
-					Utility.displayToastMessage(activity, getResources().getString(R.string.add_user_empty_balance));
-					return;
-				}
-				double balanceValue;
-				try
-				{
-					balanceValue = Double.parseDouble(balance.toString());
-				}
-				catch (NumberFormatException ignored)
-				{
-					Utility.displayToastMessage(activity, getResources().getString(R.string.add_user_empty_balance));
-					return;
+					try
+					{
+						balanceValue = Double.parseDouble(balance.toString());
+					}
+					catch (NumberFormatException ignored)
+					{
+						Utility.displayToastMessage(activity, getResources().getString(R.string.add_user_empty_balance));
+						return;
+					}
 				}
 
 				final User user = new User(0,

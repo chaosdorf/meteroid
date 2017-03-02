ccda1b39df3f1b90597478db5c46e7d1ac5541c0
Niklas Sombert <niklas@ytvwld.de>
2017-03-02 12:22:39 +0100

    Make User.active default to true.
    This is useful for old (not yet updated) instances of Mete.

--- meteroid/src/main/java/de/chaosdorf/meteroid/model/User.java
+++ meteroid/src/main/java/de/chaosdorf/meteroid/model/User.java
@@ -30,7 +30,7 @@ public class User
 	private final String name;
 	private final String email;
 	private final double balance;
-	private final boolean active;
+	private boolean active = true;
 
 	public User(final int id, final String name, final String email, final double balance, final boolean active)
 	{

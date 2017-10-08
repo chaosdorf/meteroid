6f8948834fe6a86a14a2302f6eba985ba5e29181
Niklas Sombert <niklas@ytvwld.de>
2017-10-08 14:44:28 +0200

    UserSettings: Remove unused imports.

--- meteroid/src/main/java/de/chaosdorf/meteroid/UserSettings.java
+++ meteroid/src/main/java/de/chaosdorf/meteroid/UserSettings.java
@@ -36,9 +36,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
 

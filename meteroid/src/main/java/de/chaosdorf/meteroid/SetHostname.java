chaosdorf/meteroid

commit 2a8e6fb3d68f77360e2dbc22a19b59343ef0e399

Test the connection before saving (closes #17)

meteroid/src/main/java/de/chaosdorf/meteroid/SetHostname.java
@@ -28,7 +28,9 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.ActionBar;
 import android.content.DialogInterface;
+import android.content.Intent;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableBoolean;
 import android.os.Bundle;
 import android.os.Build;
 import android.text.Editable;
@@ -37,6 +39,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.URLUtil;
+import android.widget.Toast;
 
 import de.chaosdorf.meteroid.databinding.ActivitySetHostnameBinding;
 import de.chaosdorf.meteroid.util.Config;
@@ -49,6 +52,7 @@ public class SetHostname extends AppCompatActivity
 	private Config config;
 	private Connection connection;
 	private ActivitySetHostnameBinding binding;
+	private final ObservableBoolean writable = new ObservableBoolean(false);
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState)
@@ -56,6 +60,7 @@ public class SetHostname extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		activity = this;
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_set_hostname);
+		binding.setWritable(writable);
 
 		config = Config.getInstance(getApplicationContext());
 
@@ -79,6 +84,7 @@ public class SetHostname extends AppCompatActivity
 		{
 			binding.buttonSave.setVisibility(View.GONE);
 		}
+		writable.set(true);
 	}
 
 	@Override
@@ -140,22 +146,35 @@ public class SetHostname extends AppCompatActivity
 					@Override
 					public void onClick(DialogInterface dialog, int id)
 					{
-						saveAndExit(url);
+						trySaveAndExit(url);
 					}
 				})
 				.setNegativeButton(android.R.string.cancel, null) // Do nothing on click.
 				.create().show();
 		} else {
-			saveAndExit(url);
+			trySaveAndExit(url);
 		}
 	}
 	
-	private void saveAndExit(String newHostname) {
-		config.hostname = newHostname;
-		config.apiVersion = Utility.guessApiVersion(newHostname);
-		config.save();
+	private void trySaveAndExit(String newHostname) {
+		writable.set(false);
 		connection = Connection.getInstance(config);
-		connection.reset();
-		Utility.startActivity(activity, PickUsername.class);
+		connection.check(connection.initializeRetrofit(newHostname), new Connection.CheckCallback() {
+			@Override
+			public void handleSuccess() {
+				config.hostname = newHostname;
+				config.apiVersion = Utility.guessApiVersion(newHostname);
+				config.save();
+				connection.reset();
+				Utility.startActivity(activity, PickUsername.class, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				finish();
+			}
+			
+			@Override
+			public void handleError(String message) {
+				Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+				writable.set(true);
+			}
+		});
 	}
 }

meteroid/src/main/java/de/chaosdorf/meteroid/util/Connection.java
@@ -34,7 +34,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import de.chaosdorf.meteroid.longrunningio.LongRunningIOCallback;
 import de.chaosdorf.meteroid.longrunningio.LongRunningIORequest;
 import de.chaosdorf.meteroid.longrunningio.LongRunningIOTask;
-import de.chaosdorf.meteroid.model.User;
+import de.chaosdorf.meteroid.model.Drink;
 
 public class Connection
 {
@@ -71,6 +71,24 @@ public class Connection
     {
         return api;
     }
+
+    public void check(API api, CheckCallback callback) {
+        new LongRunningIORequest<List<Drink>>(
+            new LongRunningIOCallback<List<Drink>>() {
+                @Override
+                public void displayErrorMessage(LongRunningIOTask task, String message) {
+                    callback.handleError(message);
+                }
+
+                @Override
+                public void processIOResult(LongRunningIOTask task, List<Drink> result) {
+                    callback.handleSuccess();
+                }
+            },
+            LongRunningIOTask.GET_DRINKS,
+            api.listDrinks()
+        );
+    }
     
     private void upgradeAPIversion()
     {
@@ -88,30 +106,26 @@ public class Connection
                 Log.e(TAG, "API version is configured as 'legacy', but seems like 'v1'. This is most certainly a bug.");
             }
             final String test_hostname = config.hostname + "api/v1/";
+
             final API test_api = initializeRetrofit(test_hostname);
-            new LongRunningIORequest<List<User>>(new LongRunningIOCallback<List<User>>()
-            {
-                @Override
-                public void displayErrorMessage(LongRunningIOTask task, String message)
-                {
-                    Log.w(TAG, "The server doesn't seem to support 'v1' (or the request failed): "  + message);
-                    api = initializeRetrofit(config.hostname);
-                }
-                
-                @Override
-                public void processIOResult(LongRunningIOTask task, List<User> users)
-                {
+            check(test_api, new CheckCallback() {
+                public void handleSuccess() {
                     Log.i(TAG, "The server seems to support the newer API version 'v1'. Upgrading.");
                     config.hostname = test_hostname;
                     config.apiVersion = "v1";
                     config.save();
                     api = test_api;
                 }
-            }, LongRunningIOTask.GET_USERS, test_api.listUsers());
+
+                public void handleError(String message) {
+                    Log.w(TAG, "The server doesn't seem to support 'v1' (or the request failed): " + message);
+                    api = initializeRetrofit(config.hostname);
+                }
+            });
         }
     }
     
-    private API initializeRetrofit(String url)
+    public API initializeRetrofit(String url)
     {
         Log.d(TAG, "Opening connection to " + url + "...");
         return new Retrofit.Builder()
@@ -119,5 +133,10 @@ public class Connection
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(API.class);
+    }
+
+    public interface CheckCallback {
+        public void handleError(String message);
+        public void handleSuccess();
     }
 }

meteroid/src/main/res/layout/activity_set_hostname.xml
@@ -29,6 +29,13 @@
   xmlns:tools="http://schemas.android.com/tools"
   >
 
+  <data>
+    <variable
+      name="writable"
+      type="androidx.databinding.ObservableBoolean"
+    />
+  </data>
+
   <RelativeLayout android:layout_width="fill_parent"
                 android:layout_height="fill_parent"
                 android:orientation="vertical"
@@ -57,6 +64,7 @@
 
           <com.google.android.material.textfield.TextInputEditText
               android:inputType="textUri"
+              android:enabled="@{writable}"
               android:id="@+id/hostname"
               android:text="@string/set_hostname_default_hostname"
               android:layout_height="wrap_content"
@@ -66,6 +74,7 @@
 
       <Button
           android:id="@+id/button_save"
+          android:enabled="@{writable}"
           android:layout_width="wrap_content"
           android:layout_height="wrap_content"
           android:text="@string/button_save"

commit fa9f34f3bfc8022fc6721e2a42d20e61840a71d0

Fix setting the URL
This was broken in 2a8e6fb3d68f77360e2dbc22a19b59343ef0e399.

meteroid/src/main/java/de/chaosdorf/meteroid/SetHostname.java
@@ -50,7 +50,6 @@ public class SetHostname extends AppCompatActivity
 {
 	private AppCompatActivity activity = null;
 	private Config config;
-	private Connection connection;
 	private ActivitySetHostnameBinding binding;
 	private final ObservableBoolean writable = new ObservableBoolean(false);
 
@@ -158,14 +157,13 @@ public class SetHostname extends AppCompatActivity
 	
 	private void trySaveAndExit(String newHostname) {
 		writable.set(false);
-		connection = Connection.getInstance(config);
-		connection.check(connection.initializeRetrofit(newHostname), new Connection.CheckCallback() {
+		Connection.check(Connection.initializeRetrofit(newHostname), new Connection.CheckCallback() {
 			@Override
 			public void handleSuccess() {
 				config.hostname = newHostname;
 				config.apiVersion = Utility.guessApiVersion(newHostname);
 				config.save();
-				connection.reset();
+				Connection.getInstance(config).reset();
 				Utility.startActivity(activity, PickUsername.class, Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				finish();
 			}

meteroid/src/main/java/de/chaosdorf/meteroid/util/Connection.java
@@ -72,7 +72,7 @@ public class Connection
         return api;
     }
 
-    public void check(API api, CheckCallback callback) {
+    public static void check(API api, CheckCallback callback) {
         new LongRunningIORequest<List<Drink>>(
             new LongRunningIOCallback<List<Drink>>() {
                 @Override
@@ -125,7 +125,7 @@ public class Connection
         }
     }
     
-    public API initializeRetrofit(String url)
+    public static API initializeRetrofit(String url)
     {
         Log.d(TAG, "Opening connection to " + url + "...");
         return new Retrofit.Builder()